f700ae62b946511beb8ce5a7aef0834b2ec099be
Ilya Shemetov <ilyashpb1@gmail.com>
2025-11-28 00:13:49 +0300

    add new instaction and refactoring

--- cpu/include/cpu.h
+++ cpu/include/cpu.h
@@ -3,8 +3,11 @@
 #include <array>
 #include <vector>
 #include <string.h>
+#include <assert.h>
 
 using WORD = uint32_t;
+using DWORD = uint64_t;
+
 
 class RAM
 {
@@ -47,32 +50,89 @@ public:
         R8,
         IP,
         SP,
+        FLAGS,
         R_NUM
     };
 
+    enum class Flag: uint8_t
+    {
+        Zero = 0,
+        Carry,
+        Overflow,
+        Negative
+    };
+    
     CPU(RAM& ram):
-        _ram(ram)
+    _ram(ram)
     {
     }
 
-    void Add(Register reg1, Register reg2)
+    void SetFlag(Flag flag)
     {
-        Reg(reg1) = Reg(reg1) + Reg(reg2);
+        Reg(Register::FLAGS) |= (1 << static_cast<uint32_t>(flag));
     }
 
-    void Move(Register reg1, Register reg2)
+    void ClearFlag(Flag flag)
     {
-        Reg(reg2) = Reg(reg1);
+        Reg(Register::FLAGS) &= ~(1 << static_cast<uint32_t>(flag));
     }
 
-    void Load(WORD addr, Register reg)
+    uint8_t GetFlag(Flag flag)
     {
-        Reg(reg) = _ram.Read(addr);
+        return Reg(Register::FLAGS) & (1 << static_cast<uint32_t>(flag));
     }
 
-    void Store(Register reg, WORD addr)
+    void Add(Register dst, Register reg1, Register reg2)
     {
-        _ram.Write(addr, Reg(reg));
+        WORD op1 = Reg(reg1);
+        WORD op2 = Reg(reg2);
+        DWORD wide_op1 = static_cast<DWORD>(op1);
+        DWORD wide_op2 = static_cast<DWORD>(op2);
+        
+        DWORD wide_res = wide_op1 + wide_op2;
+        WORD res = static_cast<WORD>(wide_res);
+
+        UpdateZeroFlag(res);
+        UpdateNegativeFlag(res);
+        UpdateCarryFlag(wide_res);
+
+        uint8_t op1_sign = op1 >> MSB_I;
+        uint8_t op2_sign = op2 >> MSB_I;
+        uint8_t res_sign = res >> MSB_I;
+        uint8_t overflow = ~(op1_sign ^ op2_sign) & (op1_sign ^ res_sign);
+        UpdateFlag(Flag::Overflow, overflow);
+
+        Reg(dst) = res;
+    }
+
+    void Sub(Register dst, Register reg1, Register reg2)
+    {
+        Reg(dst) = DoSub(Reg(reg1), Reg(reg2));
+    }
+
+    void Cmp(Register reg1, Register reg2)
+    {
+        (void)DoSub(Reg(reg1), Reg(reg2));
+    }
+
+    void Move(Register dst, Register src)
+    {
+        Reg(dst) = Reg(src);
+    }
+
+    void Load(Register reg1, Register addr)
+    {
+        Reg(reg1) = _ram.Read(Reg(addr));
+    }
+
+    void LoadImmediate(Register reg1, WORD data)
+    {
+        Reg(reg1) = data;
+    }
+
+    void Store(Register reg1, Register addr)
+    {
+        _ram.Write(Reg(addr), Reg(reg1));
     }
 
     void JumpZero(Register reg, WORD addr)
@@ -81,34 +141,44 @@ public:
             Reg(Register::IP) = addr;
     }
 
-    void ShiftRight(Register reg1, Register reg2)
+    void ShiftRight(Register dst, Register reg1, Register reg2)
     {
-        Reg(reg1) = Reg(reg1) >> Reg(reg2);
+        WORD tmp = Reg(reg1) >> Reg(reg2);
+        UpdateZeroFlag(tmp);
+        UpdateNegativeFlag(tmp);
+        Reg(dst) = tmp;
     }
 
-    void ShiftLeft(Register reg1, Register reg2)
+    void ShiftLeft(Register dst, Register reg1, Register reg2)
     {
-        Reg(reg1) = Reg(reg1) << Reg(reg2);
+        WORD tmp = Reg(reg1) << Reg(reg2);
+        UpdateZeroFlag(tmp);
+        UpdateNegativeFlag(tmp);
+        Reg(dst) = tmp;
     }
 
-    void OR(Register reg1, Register reg2)
+    void Or(Register dst, Register reg1, Register reg2)
     {
-        Reg(reg1) = Reg(reg1) | Reg(reg2);
+        WORD tmp = Reg(reg1) | Reg(reg2);
+        UpdateZeroFlag(tmp);
+        UpdateNegativeFlag(tmp);
+        Reg(dst) = tmp;
     }
 
-    void AND(Register reg1, Register reg2)
+    void And(Register dst, Register reg1, Register reg2)
     {
-        Reg(reg1) = Reg(reg1) & Reg(reg2);
+        WORD tmp = Reg(reg1) & Reg(reg2);
+        UpdateZeroFlag(tmp);
+        UpdateNegativeFlag(tmp);
+        Reg(dst) = tmp;
     }
 
-    void NOT(Register reg1)
+    void Xor(Register dst, Register reg1, Register reg2)
     {
-        Reg(reg1) = ~Reg(reg1);
-    }
-
-    void XOR(Register reg1, Register reg2)
-    {
-        Reg(reg1) = Reg(reg1) ^ Reg(reg2);
+        WORD tmp = Reg(reg1) ^ Reg(reg2);
+        UpdateZeroFlag(tmp);
+        UpdateNegativeFlag(tmp);
+        Reg(dst) = tmp;
     }
 
     WORD& Reg(Register reg)
@@ -117,8 +187,57 @@ public:
     }
 
 private:
+    static_assert(sizeof(DWORD) > sizeof(WORD));
+    const static WORD MSB_I = ((sizeof(WORD) * 8) - 1);
+    const static WORD CB_I = ((sizeof(WORD) * 8));
+    
     using RegisterFile = std::array<WORD, static_cast<size_t>(Register::R_NUM)>;
 
-    RegisterFile    _reg_file{ 0 };
-    RAM&            _ram;
+    RegisterFile _reg_file{ 0 };
+    RAM& _ram;
+
+    WORD DoSub(WORD op1, WORD op2)
+    {
+        DWORD wide_op1 = static_cast<DWORD>(op1);
+        DWORD wide_op2 = static_cast<DWORD>(~op2 + 1);
+        DWORD wide_res = wide_op1 + wide_op2;
+        WORD res = static_cast<WORD>(wide_res);
+
+        UpdateNegativeFlag(res);
+        UpdateZeroFlag(res);
+        UpdateCarryFlag(wide_res);
+        
+        uint8_t op1_sign = op1 >> MSB_I;
+        uint8_t op2_sign = op2 >> MSB_I;
+        uint8_t res_sign = res >> MSB_I;
+        uint8_t overflow = (op1_sign ^ op2_sign) & (op1_sign ^ res_sign);
+        UpdateFlag(Flag::Overflow, overflow);
+        
+        return res;
+    }
+
+    void UpdateFlag(Flag flag, uint8_t state)
+    {
+        if (state)
+            SetFlag(flag);
+        else
+            ClearFlag(flag);
+    }
+
+    void UpdateZeroFlag(WORD value)
+    {
+        UpdateFlag(Flag::Zero, value == 0);
+    }
+
+    void UpdateNegativeFlag(WORD value)
+    {
+        uint8_t negative = (value >> MSB_I) & 1;
+        UpdateFlag(Flag::Negative, negative);
+    }
+
+    void UpdateCarryFlag(DWORD wide_value)
+    {
+        uint8_t carry = (wide_value >> CB_I) & 1;
+        UpdateFlag(Flag::Carry, carry);
+    }
 };
--- cpu/tests/test.cpp
+++ cpu/tests/test.cpp
@@ -3,42 +3,112 @@
 
 TEST(CPUTest, AddTest)
 {
-    RAM ram{ 1024 };
-    ram.Write(0, 21);
-    ram.Write(4, 21);
-
+    RAM ram{ 0 };
     CPU cpu{ram};
-    cpu.Load(0, CPU::Register::R1);
-    cpu.Load(4, CPU::Register::R2);
+
+    cpu.LoadImmediate(CPU::Register::R1, 21);
+    cpu.LoadImmediate(CPU::Register::R2, 21);
     
     EXPECT_EQ(cpu.Reg(CPU::Register::R1), 21);
     EXPECT_EQ(cpu.Reg(CPU::Register::R2), 21);
 
-    cpu.Add(CPU::Register::R1, CPU::Register::R2);
+    cpu.Add(CPU::Register::R3, CPU::Register::R1, CPU::Register::R2);
     
-    EXPECT_EQ(cpu.Reg(CPU::Register::R1), 42);
+    EXPECT_EQ(cpu.Reg(CPU::Register::R3), 42);
+    EXPECT_EQ(cpu.Reg(CPU::Register::R1), 21);
     EXPECT_EQ(cpu.Reg(CPU::Register::R2), 21);
-
-    cpu.Store(CPU::Register::R1, 8);
-    EXPECT_EQ(ram.Read(8), 42);
 }
+
+TEST(CPUTest, AddOverflowTests)
+{
+    RAM ram{0};
+    CPU cpu{ram};
+
+    // 1. No overflow: 10 + 20 = 30
+    cpu.LoadImmediate(CPU::Register::R1, 10);
+    cpu.LoadImmediate(CPU::Register::R2, 20);
+    cpu.Add(CPU::Register::R3, CPU::Register::R1, CPU::Register::R2);
+    EXPECT_EQ(cpu.Reg(CPU::Register::R3), 30);
+    EXPECT_FALSE(cpu.GetFlag(CPU::Flag::Carry)); // no carry
+    EXPECT_FALSE(cpu.GetFlag(CPU::Flag::Overflow)); // no signed overflow
+
+    // 2. Unsigned overflow: 0xFFFFFFFF + 1 = 0x00000000
+    cpu.LoadImmediate(CPU::Register::R1, 0xFFFFFFFFu);
+    cpu.LoadImmediate(CPU::Register::R2, 1);
+    cpu.Add(CPU::Register::R3, CPU::Register::R1, CPU::Register::R2);
+    EXPECT_EQ(cpu.Reg(CPU::Register::R3), 0x00000000u);
+    EXPECT_TRUE(cpu.GetFlag(CPU::Flag::Carry));  // carry out
+    EXPECT_FALSE(cpu.GetFlag(CPU::Flag::Overflow)); // no signed overflow
+
+    // 3. Signed overflow: 0x7FFFFFFF + 1 = 0x80000000
+    cpu.LoadImmediate(CPU::Register::R1, 0x7FFFFFFF);
+    cpu.LoadImmediate(CPU::Register::R2, 1);
+    cpu.Add(CPU::Register::R3, CPU::Register::R1, CPU::Register::R2);
+    EXPECT_EQ(cpu.Reg(CPU::Register::R3), 0x80000000u);
+    EXPECT_FALSE(cpu.GetFlag(CPU::Flag::Carry)); // no unsigned carry
+    EXPECT_TRUE(cpu.GetFlag(CPU::Flag::Overflow));  // signed overflow
+
+    // 4. Signed overflow: 0x80000000 + 0x80000000 = 0x00000000
+    cpu.LoadImmediate(CPU::Register::R1, 0x80000000);
+    cpu.LoadImmediate(CPU::Register::R2, 0x80000000);
+    cpu.Add(CPU::Register::R3, CPU::Register::R1, CPU::Register::R2);
+    EXPECT_EQ(cpu.Reg(CPU::Register::R3), 0x00000000u);
+    EXPECT_TRUE(cpu.GetFlag(CPU::Flag::Carry));  // carry out
+    EXPECT_TRUE(cpu.GetFlag(CPU::Flag::Overflow));  // signed overflow
+}
+
+TEST(CPUTest, SubOverflowTests)
+{
+    RAM ram{0};
+    CPU cpu{ram};
+
+    // 1. No overflow: 20 - 10 = 10
+    cpu.LoadImmediate(CPU::Register::R1, 20);
+    cpu.LoadImmediate(CPU::Register::R2, 10);
+    cpu.Sub(CPU::Register::R3, CPU::Register::R1, CPU::Register::R2);
+    EXPECT_EQ(cpu.Reg(CPU::Register::R3), 10u);
+    EXPECT_TRUE(cpu.GetFlag(CPU::Flag::Carry));   // no borrow
+    EXPECT_FALSE(cpu.GetFlag(CPU::Flag::Overflow)); // no signed overflow
+
+    // 2. Unsigned borrow: 10 - 20 = 0xFFFFFFF6
+    cpu.LoadImmediate(CPU::Register::R1, 10);
+    cpu.LoadImmediate(CPU::Register::R2, 20);
+    cpu.Sub(CPU::Register::R3, CPU::Register::R1, CPU::Register::R2);
+    EXPECT_EQ(cpu.Reg(CPU::Register::R3), 0xFFFFFFF6u);
+    EXPECT_FALSE(cpu.GetFlag(CPU::Flag::Carry));  // borrow occurred
+    EXPECT_FALSE(cpu.GetFlag(CPU::Flag::Overflow)); // no signed overflow
+
+    // 3. Signed overflow: (-2147483648) - 1 = +2147483647
+    cpu.LoadImmediate(CPU::Register::R1, 0x80000000); // -2147483648
+    cpu.LoadImmediate(CPU::Register::R2, 1);
+    cpu.Sub(CPU::Register::R3, CPU::Register::R1, CPU::Register::R2);
+    EXPECT_EQ(cpu.Reg(CPU::Register::R3), 0x7FFFFFFFu);
+    EXPECT_TRUE(cpu.GetFlag(CPU::Flag::Carry));   // no borrow
+    EXPECT_TRUE(cpu.GetFlag(CPU::Flag::Overflow)); // signed overflow
+
+    // 4. Signed overflow: (+2147483647) - (-1) = -2147483648
+    cpu.LoadImmediate(CPU::Register::R1, 0x7FFFFFFF); // +2147483647
+    cpu.LoadImmediate(CPU::Register::R2, 0xFFFFFFFF); // -1
+    cpu.Sub(CPU::Register::R3, CPU::Register::R1, CPU::Register::R2);
+    EXPECT_EQ(cpu.Reg(CPU::Register::R3), 0x80000000u);
+    EXPECT_FALSE(cpu.GetFlag(CPU::Flag::Carry));   // no borrow
+    EXPECT_TRUE(cpu.GetFlag(CPU::Flag::Overflow)); // signed overflow
+}
+
 
 TEST(CPUTest, ShiftTest)
 {
-    RAM ram{ 1024 };
-    ram.Write(0, 0x00ff0000);
-    ram.Write(4, 8);
-
+    RAM ram{ 0 };
     CPU cpu{ram};
-    cpu.Load(0, CPU::Register::R1);
-    cpu.Load(4, CPU::Register::R2);
 
-    cpu.ShiftLeft(CPU::Register::R1, CPU::Register::R2);
-    EXPECT_EQ(cpu.Reg(CPU::Register::R1), 0xff000000);
+    cpu.LoadImmediate(CPU::Register::R1, 0x00ff0000);
+    cpu.LoadImmediate(CPU::Register::R2, 8);
 
-    cpu.Load(0, CPU::Register::R1);
-    cpu.ShiftRight(CPU::Register::R1, CPU::Register::R2);
-    EXPECT_EQ(cpu.Reg(CPU::Register::R1), 0x0000ff00);
+    cpu.ShiftLeft(CPU::Register::R3, CPU::Register::R1, CPU::Register::R2);
+    EXPECT_EQ(cpu.Reg(CPU::Register::R3), 0xff000000);
+
+    cpu.ShiftRight(CPU::Register::R3, CPU::Register::R1, CPU::Register::R2);
+    EXPECT_EQ(cpu.Reg(CPU::Register::R3), 0x0000ff00);
 }
 
 TEST(CPUTest, BitwiseTest)
@@ -48,21 +118,15 @@ TEST(CPUTest, BitwiseTest)
     ram.Write(4, 0x00ffff00);
 
     CPU cpu{ram};
-    cpu.Load(0, CPU::Register::R1);
-    cpu.Load(4, CPU::Register::R2);
+    cpu.LoadImmediate(CPU::Register::R1, 0xffff0000);
+    cpu.LoadImmediate(CPU::Register::R2, 0x00ffff00);
 
-    cpu.OR(CPU::Register::R1, CPU::Register::R2);
-    EXPECT_EQ(cpu.Reg(CPU::Register::R1), 0xffffff00);
+    cpu.Or(CPU::Register::R3, CPU::Register::R1, CPU::Register::R2);
+    EXPECT_EQ(cpu.Reg(CPU::Register::R3), 0xffffff00);
 
-    cpu.Load(0, CPU::Register::R1);
-    cpu.AND(CPU::Register::R1, CPU::Register::R2);
-    EXPECT_EQ(cpu.Reg(CPU::Register::R1), 0x00ff0000);
+    cpu.And(CPU::Register::R3, CPU::Register::R1, CPU::Register::R2);
+    EXPECT_EQ(cpu.Reg(CPU::Register::R3), 0x00ff0000);
 
-    cpu.Load(0, CPU::Register::R1);
-    cpu.XOR(CPU::Register::R1, CPU::Register::R2);
-    EXPECT_EQ(cpu.Reg(CPU::Register::R1), 0xff00ff00);
-
-    cpu.Load(0, CPU::Register::R1);
-    cpu.NOT(CPU::Register::R1);
-    EXPECT_EQ(cpu.Reg(CPU::Register::R1), 0x0000ffff);
+    cpu.Xor(CPU::Register::R3, CPU::Register::R1, CPU::Register::R2);
+    EXPECT_EQ(cpu.Reg(CPU::Register::R3), 0xff00ff00);
 }
